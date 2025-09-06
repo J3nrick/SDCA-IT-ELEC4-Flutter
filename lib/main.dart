9f8fa36b535ad90d8cf2f3db1e74eccc5a69e7f5
Jenrick Ambalong <jenerick.101@gmail.com>
2025-09-06 16:05:50 +0800

    Create home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,22 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
-void main(){
+void main() {
   runApp(App());
 }
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-Scaffold homepage = Scaffold(
-  appBar: AppBar(title: Text('Sir tapos na po')),
-  floatingActionButton: FloatingActionButton(
-    onPressed: (){
-
-    },
-    child: const Icon(Icons.add)
-),
-);
 
     return MaterialApp(
-      home: homepage,
+      home: HomeScreen(),
       theme: ThemeData(
-        appBarTheme:AppBarTheme(
-          backgroundColor: const Color.fromARGB(255, 223, 99, 181),
-      foregroundColor: Colors.white))
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.black
+        )
+      ),
     );
   }
 }
